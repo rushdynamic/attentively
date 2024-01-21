e76bf186cfa08383dc22e8cdd015e1446392729c
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-01-22 01:16:23 +0530

    fix: tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import StopWatch from './components/StopWatch';
+import './App.css';
 
 function App() {
 	return (
-		<div className="h-full w-full flex items-center justify-center">
-			<h1>Flowmodoro</h1>
-			<div className="card">
-				<p>
-					<StopWatch />
-				</p>
+		<div className="h-screen w-screen bg-gray-600 flex items-center justify-center">
+			<div className="flex flex-col items-center">
+				<span className="font-semibold text-xl text-gray-400">Flowmodoro</span>
+				<div className="font-semibold text-6xl">
+					<p>
+						<StopWatch />
+					</p>
+				</div>
 			</div>
 		</div>
 	);
