3732ce64b6104783ed0c93bbb10d1d68917b73af
rushdynamic <tameimpalafanclubpresident@gmail.com>
2024-01-29 14:55:48 +0530

    feat: add shadcn ui components

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Button } from '../../@/components/ui/button';
 import { formatTime } from '../utils/time_formatter';
 
 const useStopWatch = () => {
@@ -62,32 +63,18 @@ export default function StopWatch() {
 			</span>
 			<div>
 				{elapsedTime == 0 || paused ? (
-					<button
-						type="button"
-						className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-						onClick={startStopWatch}
-					>
+					<Button variant="default" onClick={startStopWatch}>
 						{paused ? 'Resume session' : 'Start session'}
-					</button>
+					</Button>
 				) : (
-					<>
-						<button
-							className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-							onClick={pauseStopWatch}
-						>
-							<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-								Take a break
-							</span>
-						</button>
-						<button
-							className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-							onClick={stopStopWatch}
-						>
-							<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-								Stop session
-							</span>
-						</button>
-					</>
+					<div className="flex items-center gap-2">
+						<Button variant="default" onClick={pauseStopWatch}>
+							Take a break
+						</Button>
+						<Button variant="destructive" onClick={stopStopWatch}>
+							Stop session
+						</Button>
+					</div>
 				)}
 			</div>
 		</div>
